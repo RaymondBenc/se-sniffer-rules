f09e28eac607b1c8df801cde2636324c9c007421
RaymondBenc <ray@socialengine.com>
2017-01-24 13:57:00 +0100

    Adding post package installer to composer

--- src/Composer.php
+++ src/Composer.php
@@ -2,18 +2,13 @@
 
 namespace SocialEngine\Console;
 
-use Composer\Script\Event;
 use Composer\Installer\PackageEvent;
 
 class Composer
 {
-  public static function postPackageInstall(Event $event)
+  public static function postPackageInstall(PackageEvent $event)
   {
     $path = $_SERVER['PWD'];
-    $package = $event->getComposer()->getPackage();
-
-    if ($package->getName() == 'raymondbenc/socialengine-console') {
-      copy(__DIR__ . '/../bin/socialengine', $path . '/socialengine');
-    }
+    copy(__DIR__ . '/../bin/socialengine', $path . '/socialengine');
   }
 }
