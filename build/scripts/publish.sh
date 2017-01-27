ad4c9d5b776d3e123e3fb12801dbcedb6f78cfe3
RaymondBenc <me@raybenc.com>
2017-01-27 11:33:03 +0100

    Removing release branch

--- build/scripts/publish.sh
+++ build/scripts/publish.sh
@@ -7,13 +7,7 @@ test "${TRAVIS_BRANCH}" == "master" || { echo "Skipping build, we only work with
 test "${TRAVIS_PHP_VERSION:0:3}" == "5.6" || { echo "Skipping for PHP $TRAVIS_PHP_VERSION -- only update for PHP 5.6 build."; exit 0; }
 test "${TRAVIS_PULL_REQUEST}" == false || { echo "Skipping pull request from building."; exit 0; }
 
-# Create work env
-rm -rf tmp/
-mkdir tmp/
-cd tmp/
-
 # Clone it
-git clone https://$GITHUB_USER_NAME:$GITHUB_TOKEN@github.com/RaymondBenc/socialengine-console.git .
 git config --global user.email $GITHUB_USER_EMAIL
 git config --global user.name $GITHUB_USER_NAME
 git remote set-url origin https://$GITHUB_USER_NAME:$GITHUB_TOKEN@github.com/SocialEngine/phpfox-importer.git
@@ -24,11 +18,7 @@ IFS=. components=(${CURRENT_VERSION##*-})
 MAJOR_VERSION=$((components[0]))
 MINOR_VERSION=$((components[1]+1))
 NEW_VERSION="$MAJOR_VERSION.$MINOR_VERSION"
-RELEASE_BRANCH="release/v$NEW_VERSION"
 MASTER="master"
-
-# Create a new release branch
-git checkout -b $RELEASE_BRANCH
 
 # Update composer.json
 echo "Incrementing version to: $NEW_VERSION"
@@ -37,16 +27,8 @@ composer config version "$NEW_VERSION"
 # Add new version and merge
 git add --all
 git commit -m "Incrementing version to $NEW_VERSION [$TRAVIS_BUILD_NUMBER]"
-
-git checkout $MASTER
-git merge --no-edit --no-ff $RELEASE_BRANCH
 git tag v$NEW_VERSION -m "Autobuild [$NEW_VERSION][$TRAVIS_BUILD_NUMBER]" $MASTER
 
 # Push to github
 git push origin $MASTER
-git push origin $RELEASE_BRANCH
 git push origin refs/tags/v$NEW_VERSION
-
-# Cleanup
-cd ../
-rm -rf tmp/
