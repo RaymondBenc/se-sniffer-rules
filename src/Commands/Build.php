a4efce081452aee5c22f4d17ef53cd47833bd79a
RaymondBenc <me@raybenc.com>
2017-02-04 10:48:43 +0100

    Fixing build packages access to config path.

--- src/Commands/Build.php
+++ src/Commands/Build.php
@@ -24,21 +24,26 @@ class Build extends Command
             unlink($file->getRealPath());
         }
 
-        file_put_contents($this->config->get('path') . 'application/packages/index.html', '');
+        $scaffold = APPLICATION_PATH . 'temporary/scaffold/index.html';
+        if (!is_file($scaffold)) {
+            touch($scaffold);
+        }
+
+        file_put_contents($this->getConfig('path') . 'application/packages/index.html', '');
 
         $write = function ($manifestPath) use ($packages) {
             $package = $packages->buildPackageFile($manifestPath);
             if ($package) {
-                $packageFileName = $this->config->get('path') . 'application/packages/' . $package->getKey() . '.json';
+                $packageFileName = $this->getConfig('path') . 'application/packages/' . $package->getKey() . '.json';
                 file_put_contents($packageFileName, json_encode($package->toArray(), JSON_PRETTY_PRINT));
-                $this->write(' -> ' . str_replace($this->config->get('path'), '', $packageFileName));
+                $this->write(' -> ' . str_replace($this->getConfig('path'), '', $packageFileName));
             }
         };
 
         foreach ($packages->getStructure() as $type => $info) {
             if (in_array($type, $packages->getActions())) {
                 $path = str_replace(array('/', '\\'), DIRECTORY_SEPARATOR, trim($info['path'], '/\\'));
-                $path = $this->config->get('path') . $path;
+                $path = $this->getConfig('path') . $path;
 
                 if (!$info['array']) {
                     $manifest = str_replace(array('/', '\\'), DIRECTORY_SEPARATOR, trim($info['manifest']));
